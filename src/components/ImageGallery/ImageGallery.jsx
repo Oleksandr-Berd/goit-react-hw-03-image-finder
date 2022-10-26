94b813a49265c340e0958c6fc1568b415e41ff25
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-10-26 14:05:06 +0300

    1

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,20 +1,29 @@
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Component } from 'react';
 import css from './ImageGallery.module.css';
+import { url } from 'components/Constant/Url';
+import { key } from 'components/Constant/Key';
 
 class ImageGallery extends Component {
+  state = {
+    image: null,
+  };
+
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.imageName !== this.props.imageName) {
-      console.log(prevProps.imageName);
-      console.log(this.props.imageName);
-      console.log('SMTh happens');
+      fetch(
+        `${url}?q=${this.props.imageName}&page=1&key=${key}&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then(res => res.json())
+        .then(image => this.setState({ image }));
     }
   }
 
   render() {
     return (
       <ul className={css.gallery}>
-        {this.props.imageName && <ImageGalleryItem />}
+        {this.state.image &&
+          Object.values(this.state.image)[2].map(el => console.log(el))}
       </ul>
     );
   }
