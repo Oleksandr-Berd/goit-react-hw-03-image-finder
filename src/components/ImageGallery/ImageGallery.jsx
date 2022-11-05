5cb7040c2c028f0a96665a9a3889454b6cef2e01
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-11-05 11:36:04 +0200

    1

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,7 +1,7 @@
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Dna } from 'react-loader-spinner';
 import Button from 'components/Button/Button';
-
+import axios from 'axios';
 import { Component } from 'react';
 import css from './ImageGallery.module.css';
 import { url } from 'components/Constant/Url';
@@ -9,7 +9,7 @@ import { key } from 'components/Constant/Key';
 
 class ImageGallery extends Component {
   state = {
-    image: null,
+    image: [],
     loading: false,
     error: null,
     page: 1,
@@ -21,25 +21,23 @@ class ImageGallery extends Component {
     }));
   };
 
-  componentDidUpdate(prevProps, prevState) {
+  async componentDidUpdate(prevProps, prevState) {
     if (
       prevProps.imageName !== this.props.imageName ||
       prevState.page !== this.state.page
     ) {
-      this.setState({ loading: true, image: null });
-
-      setTimeout(() => {
-        fetch(
-          `${url}?q=${this.props.imageName}&page=${this.state.page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`
-        )
-          .then(res => res.json())
-          .then(image => this.setState({ image }))
-          .catch(error => this.setState({ error }))
-          .finally(this.setState({ loading: false }));
-      }, 1000);
+      this.setState({ loading: true });
+      const response = await axios.get(
+        `${url}?q=${this.props.imageName}&page=${this.state.page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`
+      );
+      console.log(response.data.hits);
+      this.setState(prevState => ({
+        image: [...prevState.image, ...response.data.hits],
+        loading: false,
+      }));
     }
-    console.log(prevState.page);
-    console.log(this.state.page);
+    console.log(prevState.image);
+    console.log(this.state.image);
   }
 
   render() {
@@ -60,18 +58,17 @@ class ImageGallery extends Component {
           />
         )}
         {!imageName && <div>Enter some data</div>}
-        {image &&
-          Object.values(image)[2].map(
-            ({ id, webformatURL, tags, largeImageURL }) => (
+        {image.length > 0
+          ? image.map(({ id, webformatURL, tags, largeImageURL }) => (
               <ImageGalleryItem
                 key={id}
                 webformatURL={webformatURL}
                 tags={tags}
                 largeImageURL={largeImageURL}
               />
-            )
-          )}
-        {image && <Button loadMore={this.loadMore} />}
+            ))
+          : null}
+        {image.length > 0 && <Button loadMore={this.loadMore} />}
       </ul>
     );
   }
